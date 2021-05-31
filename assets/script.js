d6749ac9d7a82ec8bf08f05704ba8df056be1765
Alexey Kuzmis <alexeykuzmis@Alexeys-MacBook-Pro.local>
2021-05-31 14:16:51 +0100

    started api js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+const secret_api = ' ENTER YOUR CODE';
+const bypass_cors_url = 'https://cors-anywhere.herokuapp.com';
+const api_uri = 'https://geo.ipify.org/api/';
+let current_version = 'v1';
+
+
+
+
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,8 +21,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
+
 searchButton.addEventListener('click', () => {
   const inputValue = searchInput.value;
   alert(inputValue);
