3b5e2eff6e7206d09e3d275fe4a91ace4dd2cc09
Alexey Kuzmis <alexeykuzmis@Alexeys-MacBook-Pro.local>
2021-05-31 20:08:16 +0100

    finalised js

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,79 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiYWxleGV5bWFwIiwiYSI6ImNrcDhvcWVvNDBheXEyeW9nd3hiYTRwYm8ifQ.3MQpyGhVFK3KDO0wVUr7og'
-}).addTo(mymap);
-
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-let searchIp = document.getElementById('search-ip')
-let searchBtn = document.getElementById('search-button')
-let input
-// function searchRef(){
-//     console.log(searchIp.value)
-//     const inputValue = searchIp.value;
-//     return inputValue
-// }
-
-// searchBtn.addEventListener('click', searchRef)
-
-searchBtn.addEventListener('click', () =>{ let input = searchRef() });
-
-console.log(input)
 
 
+// pull from different file
+const secret_api ='x'
+const bypass_cors_url = 'https://cors-anywhere.herokuapp.com/'
+const api_uri = 'https://geo.ipify.org/api/'
+let current_verion = 'v1'
 
-var ip = "8.8.8.8";
-    var api_key = "at_hqno4AyaM81PwU3wc5lGtnqVcadHM ";
-    $(function () {
-        $.ajax({
+// form elements
+let searchIp = document.getElementById('search-ip');
+let searchBtn = document.getElementById('search-button');
 
-            url: "https://geo.ipify.org/api/v1",
-            data: {apiKey: api_key, ipAddress: ip},
-            success: function(data) {
-                    $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
-            }
-        });
-    });
+// elements to update 
+let current_ip = document.getElementById('ip-address');
+let current_town = document.getElementById('ip-location');
+let current_zone = document.getElementById('ip-time');
+let current_isp = document.getElementById('ip-name');
 
+
+const headers_option = {
+  headers: {
+      'Access-Control-Allow-Origin': '*',
+  }
+}
+
+
+const map = L.map('display-map', {
+  'center': [0,0],
+  'zoom': 0,
+  'layers': [
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        })
+  ]
+})
+
+//creates a marker and map view with a variable map and updatE_marker
+
+updateMarker =(update_marker = [-42,42]) =>{
+  map.setView(update_marker, 13) //setting the view
+  L.marker(update_marker).addTo(map); // settting the marker  
+
+}
+
+getIPDetails = (default_ip) => {
+  if(default_ip == undefined){
+      var ip_url = `${bypass_cors_url}${api_uri}${current_verion}?apiKey=${secret_api}`
+  }
+  else {
+      var ip_url = `${bypass_cors_url}${api_uri}${current_verion}?apiKey=${secret_api}&ipAddress=${default_ip}`
+  }
+  fetch(ip_url, headers_option)
+  .then(result => result.json())
+  .then(data => {
+    current_ip.innerHTML = data.ip;
+    current_town.innerHTML = `${data.location.city} ${data.location.country} ${data.location.postalCode}`;
+    current_zone.innerHTML = data.location.timezone;
+    current_isp.innerHTML = data.isp;
+
+    updateMarker(data.location.lat, data.location.lng);
+  })
+  .catch(error => {
+    alert("Unable to get IP details")
+    console.log(error)
+})
+}
+
+getIPDetails();
+document.addEventListener('load', updateMarker);
+
+searchBtn.addEventListener('click', e =>{
+  e.preventDefault()
+  if (searchIp.value != '' && searchIp.value != null) {
+    getIPDetails(searchIp.value)
+    return
+  }
+    alert("Please enter a valid IP address");
+})
 
